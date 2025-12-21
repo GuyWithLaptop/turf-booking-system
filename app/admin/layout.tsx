e36dd553e5cb842c7da900e30f61455fb8acdb68
Zaidkhan <plainpresence@gmail.com>
2025-12-21 19:34:42 +0530

    Trigger redeploy - force cache invalidation

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,5 +7,9 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  );
 }
